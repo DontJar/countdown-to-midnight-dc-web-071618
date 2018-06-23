9a0604fffb5f13579b11ce1f075ea8d8cd9c12a3
Jon Schulman <schulman99@gmail.com>
2018-06-23 12:42:28 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -11,4 +11,13 @@ def countdown(n)
 end
 
 def countdown_with_sleep(n)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    sleep(1)
+    n-=1
+  end
+  if n == 0
+    return "HAPPY NEW YEAR!"
+  end
+end
 end
